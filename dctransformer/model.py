01f7b16a9511e40a9f219a33c01dc278255c8bba
wushidiguo <wukai1207@gmail.com>
2023-02-18 10:58:52 +0800

    repair wrong model decode padding mask

--- dctransformer/model.py
+++ dctransformer/model.py
@@ -7,33 +7,15 @@ from einops import rearrange
 from .sparse import DctCompress, SparseEncoder
 
 
-class Embedding(nn.Module):
-    def __init__(self, vocab_size, embed_size=512, dropout=0.1):
-        super().__init__()
-        self.embedding = nn.Embedding(vocab_size, embed_size, padding_idx=0)
-        self.dropout = dropout
-    
-    def forward(self, x):
-        return F.dropout(self.embedding(x), self.dropout)
-
-
-class ChunkEmbedding(nn.Module):
-    def __init__(self, max_nchunk, embed_size=512, dropout=0.1):
-        super().__init__()
-        self.embedding = nn.Embedding(max_nchunk, embed_size)
-        self.dropout = dropout
-    
-    def forward(self, x):
-        return F.dropout(self.embedding(x), self.dropout)
-
 
 class AttentionLayer(nn.Module):
-    def __init__(self, d_model, heads):
+    def __init__(self, d_model, heads, dropout):
         super().__init__()
         assert d_model % heads == 0, "d_model is not divisable by heads"
         self.d_model = d_model
         self.heads = heads
         self.d_k = d_model // heads
+        self.dropout = nn.Dropout(dropout)
         self.proj_q = nn.Linear(d_model, d_model)
         self.proj_k = nn.Linear(d_model, d_model)
         self.proj_v = nn.Linear(d_model, d_model)
@@ -49,7 +31,7 @@ class AttentionLayer(nn.Module):
         score = torch.einsum("bphd, bqhd -> bhpq", q, k) / math.sqrt(self.d_k)
         if mask is not None:
             score = torch.masked_fill(score, mask != 1, float("-inf"))
-        score = F.softmax(score, dim=-1)
+        score = self.dropout(F.softmax(score, dim=-1))
         attn = torch.einsum("bhpq, bqhd -> bhpd", score, v)
         attn = rearrange(attn, "b h p d -> b p (h d)")
         return self.linear(attn)
@@ -61,13 +43,14 @@ class FeedForward(nn.Module):
         self.d_model = d_model
         self.dropout = nn.Dropout(dropout)
         self.linear1 = nn.Linear(d_model, 4 * d_model)
-        self.gelu = nn.GELU()
+        self.gelu1 = nn.GELU()
         self.linear2 = nn.Linear(4 * d_model, d_model)
+        self.gelu2 = nn.GELU()
 
     def forward(self, x):
-        x = self.gelu(self.linear1(x))
+        x = self.gelu1(self.linear1(x))
         x = self.dropout(x)
-        return self.linear2(x)
+        return self.gelu2(self.linear2(x))
 
 
 class ResiduleBlock(nn.Module):
@@ -76,9 +59,10 @@ class ResiduleBlock(nn.Module):
         self.fn = fn
         self.dropout = nn.Dropout(dropout)
         self.layernorm = nn.LayerNorm(d_model)
+        self.resweight = nn.Parameter(torch.tensor([0], dtype=torch.float))
 
     def forward(self, x, *args, **kwargs):
-        return x + self.dropout(self.fn(self.layernorm(x), *args, **kwargs))
+        return x + self.dropout(self.resweight * self.fn(self.layernorm(x), *args, **kwargs))
 
 
 class Encoder(nn.Module):
@@ -88,18 +72,16 @@ class Encoder(nn.Module):
         self.dropout = dropout
 
         self.downsample = nn.Conv2d(in_channel, d_model, downsample, downsample)
-        # self.proj = nn.Linear(in_channel, d_model)
         self.attns = nn.ModuleList()
         self.ffs = nn.ModuleList()
         for i in range(n_layer):
-            self.attns.append(ResiduleBlock(AttentionLayer(d_model, heads), d_model, dropout))
+            self.attns.append(ResiduleBlock(AttentionLayer(d_model, heads, dropout), d_model, dropout))
             self.ffs.append(ResiduleBlock(FeedForward(d_model, dropout), d_model, dropout))
 
     def forward(self, x):
         x = x.to(torch.float)
         x = F.gelu(self.downsample(x))
         x = rearrange(x, "b c h w -> b (h w) c")
-        # x = self.proj(x)
         for attn, ff in zip(self.attns, self.ffs):
             x = attn(x, x, x)
             x = ff(x)
@@ -117,8 +99,8 @@ class Decoder(nn.Module):
         self.cross_attns = nn.ModuleList()
         self.ffs = nn.ModuleList()
         for i in range(n_layer):
-            self.self_attns.append(ResiduleBlock(AttentionLayer(d_model, heads), d_model, dropout))
-            self.cross_attns.append(ResiduleBlock(AttentionLayer(d_model, heads), d_model, dropout))
+            self.self_attns.append(ResiduleBlock(AttentionLayer(d_model, heads, dropout), d_model, dropout))
+            self.cross_attns.append(ResiduleBlock(AttentionLayer(d_model, heads, dropout), d_model, dropout))
             self.ffs.append(ResiduleBlock(FeedForward(d_model, dropout), d_model, dropout))
         self.generator = nn.Linear(d_model, num_cls)
 
@@ -152,10 +134,10 @@ class DCTransformer(nn.Module):
         self.chunk_size = chunk_size
         self.max_nchunk = max_nchunk
         self.decode_size = (block_size ** 2 * 3, h // block_size, w // block_size)
-        self.chn_embedding = Embedding(chn_ncls, d_model, dropout)
-        self.pos_embedding = Embedding(pos_ncls, d_model, dropout)
-        self.val_embedding = Embedding(val_ncls, d_model, dropout)
-        self.chunk_embedding = ChunkEmbedding(max_nchunk, d_model, dropout)
+        self.chn_embedding = nn.Embedding(chn_ncls, d_model, padding_idx=0)
+        self.pos_embedding = nn.Embedding(pos_ncls, d_model, padding_idx=0)
+        self.val_embedding = nn.Embedding(val_ncls, d_model, padding_idx=0)
+        self.chunk_embedding = nn.Embedding(max_nchunk, d_model)
         self.encoder = Encoder(block_size ** 2 * 3, encoder_downsample, nlayer, dropout, d_model, nheads)
         self.chn_decoder = Decoder(nlayer, dropout, d_model, nheads, chn_ncls)
         self.pos_decoder = Decoder(nlayer, dropout, d_model, nheads, pos_ncls)
@@ -164,12 +146,9 @@ class DCTransformer(nn.Module):
         self.init_parameters()
 
     def decode(self, chunks, priors, chunk_ids, return_digits=True):
-        device = chunks.device
-
         chn = chunks[:, 0, :] # (batch, seq_len + 1)
         pos = chunks[:, 1, :]
         val = chunks[:, 2, :]
-        # padding_mask = (chn[:, :-1] != 0)[:, None, :, None].to(device)   # (batch_size, 1, seq_len, 1)
 
         b, _, h, w = priors.shape 
         memory = self.encoder(priors)
@@ -190,8 +169,8 @@ class DCTransformer(nn.Module):
 
         pos_in_memory = pos_y_in_memory * w_after_downsample + pos_x_in_memory
 
-        pos_mask = pos_in_memory == 0
-        pos_in_memory[pos_in_memory < 0] = 0    # paddings and eos become zeros here, we will take care of this later
+        pos_mask = pos_in_memory < 0
+        pos_in_memory[pos_mask] = 0    # paddings and eos become zeros here, we will take care of this later
 
         gather_val = torch.gather(memory, 1, pos_in_memory.unsqueeze(-1).repeat(1, 1, self.d_model))
         gather_val = torch.masked_fill(gather_val, pos_mask[:, :, None], value=0)    # paddings and eos are all made zero vectors
@@ -228,8 +207,6 @@ class DCTransformer(nn.Module):
 
     @torch.no_grad()
     def greedy(self, chunks, priors, chunk_ids):
-        device = chunks.device
-    
         chn = chunks[:, 0, :]
         pos = chunks[:, 1, :]
         val = chunks[:, 2, :]
@@ -274,7 +251,7 @@ class DCTransformer(nn.Module):
         for i in range(self.chunk_size):
             chn_next, pos_next, val_next = self.greedy(chunks, priors, chunk_ids)
             item = torch.cat([chn_next, pos_next, val_next], dim=1)[:, :, None]
-            if (item == 2).any():
+            if ((item == 2) | (item == 0)).any():
                 return chunks[:, :, 1:], True
             chunks = torch.cat([chunks, item], dim=-1)
 
